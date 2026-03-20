1fdc4c826b55b3525d4344f52432402dc910ca75
Abhinay Pandey <abhinaypandey02@gmail.com>
2026-03-20 19:24:16 +0530

    seo setup

---
+++ lib/seo.ts
@@ -0,0 +1,8 @@
+import { setupSEO } from "naystack/utils/client";
+
+export const getSEO = setupSEO({
+  title: "ONE SOGA - The Revolution",
+  description: "The movement that united the NCR. Official merch for the revolution.",
+  siteName: "ONE SOGA",
+  themeColor: "#5b9364",
+});
