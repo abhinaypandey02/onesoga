69576e40a09ba82a17e52811e3e57e2ee4b5a57f
Abhinay Pandey <abhinaypandey02@gmail.com>
2026-03-21 14:14:25 +0530

    fix

--- data/products.ts
+++ data/products.ts
@@ -1,5 +1,5 @@
 import { Product } from "../types";
-import productsJson from "../../data/products.json";
+import productsJson from "./products.json";
 
 const products: Product[] = productsJson as Product[];
 
