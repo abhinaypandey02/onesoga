c6828870d6dae9a0d3df893cc56323425280936c
Abhinay Pandey <abhinaypandey02@gmail.com>
2026-03-22 15:55:29 +0000

    try

--- app/api/razorpay/shipping/route.ts
+++ app/api/razorpay/shipping/route.ts
@@ -23,22 +23,33 @@ export const POST = async (req: NextRequest)=>{
     }, null ,2))
     
     return NextResponse.json({
-        addresses:data.addresses.map((address:object)=>({
+  "addresses": [
+    {
       "id": "0",
       "zipcode": "560000",
       "state_code": "KA",
       "country": "IN",
       "shipping_methods": [
         {
-          "id": "0",
-          "description": "Delivery within a week",
-          "name": "BlueDart Shipping",
+          "id": "1",
+          "description": "Free shipping",
+          "name": "Delivery within 5 days",
           "serviceable": true,
-          "shipping_fee": 5000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+          "shipping_fee": 1000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
+          "cod": true,
+          "cod_fee": 1000 // in paise. Here 1000 = 1000 paise, which equals to ₹10
+        },
+        {
+          "id": "2",
+          "description": "Standard Delivery",
+          "name": "Delivered on the same day",
+          "serviceable": true,
+          "shipping_fee": 1000, // in paise. Here 1000 = 1000 paise, which equals to ₹10
           "cod": false,
           "cod_fee": 0 // in paise. Here 1000 = 1000 paise, which equals to ₹10
-        },
+        }
       ]
-}))
-    })
+    }
+  ]
+})
 }
