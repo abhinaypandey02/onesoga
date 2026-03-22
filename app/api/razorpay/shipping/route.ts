83ce79a7d1fc3b87f97b8cd7b9f011126e9bd0c0
Abhinay Pandey <abhinaypandey02@gmail.com>
2026-03-23 00:23:42 +0530

    cleanup

--- app/api/razorpay/shipping/route.ts
+++ app/api/razorpay/shipping/route.ts
@@ -1,30 +1,23 @@
 import { NextRequest, NextResponse } from "next/server"
+import { DELIVERY_FEE } from "@/lib/checkout/constants";
 
 export const POST = async (req: NextRequest)=>{
     const data = await req.json()
-    console.log("data", JSON.stringify(data,null,2))
-    const response = {
-        order_id: data.order_id,
-        razorpay_order_id: data.razorpay_order_id,
+    return NextResponse.json({
         addresses:data.addresses.map((address:object)=>({
       ...address,
       "serviceable": true,
-      "cod": false,
-      "cod_fee": 0,
-      "shipping_fee": 5000,
       "shipping_methods": [
         {
           "id": "1",
           "description": "BlueDart Shipping",
           "name": "Delivery within a week",
           "serviceable": true,
-          "shipping_fee": 5000,
+          "shipping_fee": DELIVERY_FEE*100,
           "cod": false,
           "cod_fee": 0
         },
       ]
 }))
-    }
-    console.log("response", JSON.stringify(response,null,2))
-    return NextResponse.json(response)
+    })
 }
