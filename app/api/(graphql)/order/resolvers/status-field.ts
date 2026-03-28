0c060080adbf80c1621a93890ab2d4505c8a72f7
Abhinay Pandey <abhinaypandey02@gmail.com>
2026-03-28 22:26:20 +0530

    tracking

--- app/api/(graphql)/order/resolvers/status-field.ts
+++ app/api/(graphql)/order/resolvers/status-field.ts
@@ -1,22 +1,34 @@
 import {field} from "naystack/graphql";
-import {OrderDB} from "@/app/api/(graphql)/order/db";
+import {OrderDB, OrderTable} from "@/app/api/(graphql)/order/db";
 import { getQikinkOrder} from "@/app/api/lib/qikink";
+import { db } from "@/app/api/lib/db";
+import { eq } from "drizzle-orm";
+
 
 export default field(async (order: OrderDB) => {
-  if (order.paid === null||!order.qikinkId) {
+  if (order.paid === null ||!order.qikinkId) {
     return "Pending";
   }
 
-  if (order.paid === false) {
-    return "Refunded";
-  }
+  if (order.paid === false) return "Refunded";
 
   const qikinkOrder = await getQikinkOrder(order.qikinkId);
+
   if (!qikinkOrder) {
     return "Processing";
   }
 
-  return qikinkOrder.status || "Processing";
+  if(!qikinkOrder.shipping?.awb) return "Placed";
+
+  if(qikinkOrder.shipping.tracking_link && !order.trackingLink){
+    await db.update(OrderTable).set({
+      trackingLink: qikinkOrder.shipping.tracking_link
+    }).where(eq(OrderTable.id, order.id))
+  }
+
+  if(qikinkOrder.status !== "Delivered") return "Shipped";
+
+  return qikinkOrder.status || "Placed";
 }, {
   output: String
 })
